9bbbdaa226e7e65bbfa40cf2de4626e6bdb76b6b
progawski <rogawski.piotr@gmail.com>
2020-06-03 10:19:46 +0200

    Smooth scrolling fix

--- scripts/main.js
+++ scripts/main.js
@@ -79,7 +79,7 @@ $(document).ready(function(){
 
     // Smooth auto-scrolling
 
-    $("a[href^='#']").click(function() {
+    $("a[href^='#']").click(function(e) {
         e.preventDefault(); 
         if($(this).attr("href") == "#top"){
             $("body, html").animate({scrollTop: 0}, 1000, 'swing');
